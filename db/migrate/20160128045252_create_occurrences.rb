6f23fe410ca6e970001e64c2817784086817ba4f
Kit Langton <kitlangton@gmail.com>
2016-01-28 00:34:54 -0500

    Alter migration

--- db/migrate/20160128045252_create_occurrences.rb
+++ db/migrate/20160128045252_create_occurrences.rb
@@ -1,7 +1,7 @@
 class CreateOccurrences < ActiveRecord::Migration
   def change
     create_table :occurrences do |t|
-      t.references :occurrable, index: true, foreign_key: true
+      t.integer :occurrable_id, index: true
       t.references :article, index: true, foreign_key: true
       t.string :occurrable_type
 
